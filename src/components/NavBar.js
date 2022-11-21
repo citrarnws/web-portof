4a312afcbf12db19ccab893d94e8587559e0c8d9
citraregita <citraregitanws@gmail.com>
2022-11-21 15:17:41 +0700

    active link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Navbar, Container } from "react-bootstrap";
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
 
     return (
-        <Navbar expand="logo">
+        <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
             <Container>
                 <Navbar.Brand href="#home">
                     <img src={''} alt="C|Citra"/>
@@ -15,10 +30,10 @@ export const NavBar = () => {
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#profile">Profile</Nav.Link>
-                    <Nav.Link href="#skills">Skills</Nav.Link>
-                    <Nav.Link href="#producs">Producs</Nav.Link>
+                    <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link': 'navbar-link'}>Home</Nav.Link>
+                    <Nav.Link href="#profile" className={activeLink === 'profile' ? 'active navbar-link': 'navbar-link'}>Profile</Nav.Link>
+                    <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link': 'navbar-link'}>Skills</Nav.Link>
+                    <Nav.Link href="#producs" className={activeLink === 'producs' ? 'active navbar-link': 'navbar-link'}>Producs</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
